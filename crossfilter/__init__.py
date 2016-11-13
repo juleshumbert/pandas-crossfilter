3184c5f860e1a13b86c00d83acda783522cf91d1
Jules Humbert <jules.humbert@gmail.com>
2016-11-13 21:49:35 +0100

    Added new features

--- crossfilter/__init__.py
+++ crossfilter/__init__.py
@@ -1,5 +1,11 @@
 import uuid
 from IPython.display import HTML, Javascript, display
+#        'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/4.3.0/d3.min',
+#        'crossfilter': '//cdnjs.cloudflare.com/ajax/libs/crossfilter/1.3.12/crossfilter.min',
+#        'dc': '//cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.min',
+#        'reductio': '//cdnjs.cloudflare.com/ajax/libs/reductio/0.6.3/reductio.min',
+
+
 
 
 def load_resources():
@@ -7,7 +13,7 @@ def load_resources():
     paths: {
         'd3': '//cdnjs.cloudflare.com/ajax/libs/d3/3.5.16/d3.min',
         'crossfilter': '//cdnjs.cloudflare.com/ajax/libs/crossfilter/1.3.12/crossfilter.min',
-        'dc': '//cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.min',
+        'dc': '//cdnjs.cloudflare.com/ajax/libs/dc/2.0.0-beta.32/dc',
     },
     shim: {
         'crossfilter': {
@@ -18,27 +24,31 @@ def load_resources():
 });"""), HTML('<link href="https://cdnjs.cloudflare.com/ajax/libs/dc/1.7.5/dc.min.css" rel="stylesheet" type="text/css">'))
 
 
+
 def dataframe_as_js(df, name='crossfilterData'):
     return Javascript("window.{name} = {json};".format(name=name, json=df.to_json(orient='records')))
 
 
+
 class Crossfilter:
-    def __init__(self, df, graphs=None, width=450, height=250):
+    def __init__(self, df, graph_types, axes, reducers, dim_reducer, sizes, graphs=None):
         self.df = df
         self.graphs = graphs
-        self.width = width
-        self.height = height
         if not self.graphs:
-            self.graphs = [self.default_graph(col_name) for col_name in self.df.columns]
+            self.graphs = [self.default_graph(ax, gr, red, dim_red, s) for (ax, gr, red, dim_red, s) in zip(axes, graph_types, reducers, dim_reducer, sizes)]
 
-    def default_graph(self, col_name):
+    def default_graph(self, axes, graph_type, red, dim_red, s):
         #import pdb; pdb.set_trace()
-        cat_columns = self.df.select_dtypes(include=['category']).columns
-        if col_name in cat_columns:
-            return RowChart(col_name, width=self.width, height=self.height)
-        else:
-            return BarChart(col_name, width=self.width, height=self.height)
-
+        if graph_type == 'RowChart':
+            return RowChart(axes, red, dim_red, width=s[0], height=s[1])
+        elif graph_type == 'BarChart':
+            return BarChart(axes, red, dim_red, width=s[0], height=s[1])
+        elif graph_type == 'LineChart':
+            return LineChart(axes, red, dim_red, width=s[0], height=s[1])
+        elif graph_type == 'ScatterPlot':
+            return ScatterPlot(axes, red, dim_red, width=s[0], height=s[1])
+        elif graph_type == 'PieChart':
+            return PieChart(axes, red, dim_red, width=s[0], height=s[1])
 
     def _repr_javascript_(self):
         guid = uuid.uuid4()
@@ -51,7 +61,11 @@ class Crossfilter:
     require(['d3', 'crossfilter', 'dc'], function(d3, crossfilter, dc) {
         var pluck = function(prop) {
             return function(d) { return d[prop]; };
-        }
+        };
+        var pluck2 = function(prop1, prop2) {
+            return function(d) { return [+d[prop1], +d[prop2]]; };
+        };
+        
         var crossfilterData = {json};
         var cf = crossfilter(crossfilterData);"""
         js += Summary()._repr_javascript_()
@@ -64,7 +78,8 @@ class Crossfilter:
         return js.replace('{json}', df.to_json(orient='records')).replace('{uuid}', str(guid))
 
 
-class Chart:
+
+class Chart(object):
     def __init__(self, crossfilter_name='cf'):
         self.crossfilter_name = crossfilter_name
 
@@ -84,12 +99,84 @@ class Summary(Chart):
 
 
 class ProperyChart(Chart):
-    def __init__(self, property, crossfilter_name='cf', width=450, height=250):
-        super().__init__(crossfilter_name)
+    def __init__(self, property, reducer, dim_reducer, crossfilter_name='cf', width=450, height=250):
+        super(ProperyChart, self).__init__(crossfilter_name)
         self.property = property
+        self.reducer = reducer
+        self.dim_reducer= dim_reducer
         self.width = width
         self.height = height
+        self.reducerjs = self.return_reducer()
+        self.value_accessorjs = self.return_value_accessor()
 
+    def return_value_accessor(self):
+        if self.reducer == 'None':
+            return ''
+        elif self.reducer == 'Count':
+            return ''
+        elif self.reducer == 'Sum':
+            return ''
+        elif self.reducer == 'Mean':
+            return '.valueAccessor(function (p) {return p.value.averages;});'
+        elif self.reducer == 'CumulativeSum':
+            return '.valueAccessor(function (p) {return p.value;});'
+        elif self.reducer == 'CumulativeCount':
+            return '.valueAccessor(function (p) {return p.value;});'
+
+    def return_reducer(self):
+        if self.reducer == 'None':
+            return 'var group = dim.group()'
+        elif self.reducer == 'Count':
+            return 'var group = dim.group().reduceCount()'
+        elif self.reducer == 'Sum':
+            return 'var group = dim.group().reduceSum(function(d) {return d["{dim_reducer}"];})'.replace('{dim_reducer}', self.dim_reducer)
+
+        elif self.reducer == 'Mean':
+            return """ var group = dim.group().reduce(
+function(p,v) {
+      ++p.count
+      p.sums += v["{dim_reducer}"];
+      p.averages = (p.count === 0) ? 0 : p.sums/p.count; 
+    return p;
+  }, 
+
+function(p,v) {
+      --p.count
+      p.sums -= v["{dim_reducer}"];
+      p.averages = (p.count === 0) ? 0 : p.sums/p.count;
+    return p;
+  },
+
+function reduceInitAvg() {
+  return {count:0, sums:0, averages:0};
+} )""".replace('{dim_reducer}', self.dim_reducer)
+
+        elif self.reducer == 'CumulativeCount':
+            return """var _group = dim.group().reduceCount(); 
+var group = { 
+    all:function () { 
+     var cumulate = 0; 
+     var g = []; 
+     _group.all().forEach(function(d,i) { 
+       cumulate += d.value; 
+       g.push({key:d.key,value:cumulate}) 
+     }); 
+     return g; 
+    } 
+  }; """
+        elif self.reducer == 'CumulativeSum':
+            return """var _group = dim.group().reduceSum(function(d) {return +d["{dim_reducer}"];}); 
+var group = { 
+    all:function () { 
+     var cumulate = 0; 
+     var g = []; 
+     _group.all().forEach(function(d,i) { 
+       cumulate += d.value; 
+       g.push({key:d.key,value:cumulate}) 
+     }); 
+     return g; 
+    } 
+  }; """.replace('{dim_reducer}', self.dim_reducer)
 
 class BarChart(ProperyChart):
     def _repr_javascript_(self):
@@ -100,7 +187,7 @@ class BarChart(ProperyChart):
         element.append('<div style="float: left;" id="' + chartId + '"><strong>' + prop + '</strong>' +
                        '<div style="clear: both;"></div></div>');
         var dim = {cf}.dimension(pluck(prop));
-        var group = dim.group().reduceCount();
+        {reducer}
         var min = dim.bottom(1)[0][prop];
         var max = dim.top(1)[0][prop] + 1;
         var chart = dc.barChart("#" + chartId);
@@ -108,9 +195,10 @@ class BarChart(ProperyChart):
             .x(d3.scale.linear().domain([min, max]))
             .xUnits(dc.units.integers)
             .elasticY(true)
-            .width({width}).height({height});
+            .width({width}).height({height}){valAcc};
         """.replace('{cf}', self.crossfilter_name).replace("{prop}", self.property)\
-            .replace('{width}', str(self.width)).replace('{height}', str(self.height))
+            .replace('{width}', str(self.width)).replace('{height}', str(self.height))\
+             .replace('{reducer}', self.reducerjs).replace('{valAcc}', self.value_accessorjs)
 
 
 class RowChart(ProperyChart):
@@ -122,14 +210,77 @@ class RowChart(ProperyChart):
         element.append('<div style="float: left;" id="' + chartId + '"><strong>' + prop + '</strong>' +
                        '<div style="clear: both;"></div></div>');
         var dim = {cf}.dimension(pluck(prop));
-        var group = dim.group().reduceCount();
+        {reducer}
         var min = dim.bottom(1)[0][prop];
         var max = dim.top(1)[0][prop] + 1;
         var chart = dc.rowChart("#" + chartId);
-        chart.dimension(dim).group(group).width({width}).height({height});
+        chart.dimension(dim).group(group).width({width}).height({height}).elasticX(true){valAcc};
         """.replace('{cf}', self.crossfilter_name).replace("{prop}", self.property)\
-            .replace('{width}', str(self.width)).replace('{height}', str(self.height))
+            .replace('{width}', str(self.width)).replace('{height}', str(self.height))\
+            .replace('{reducer}', self.reducerjs).replace('{valAcc}', self.value_accessorjs)
 
+class ScatterPlot(ProperyChart):
+    def _repr_javascript_(self):
+        return """
+        var prop1 = "{prop1}";
+        var prop2 = "{prop2}";
+        var propId = prop1.replace(".", "_")+prop2.replace(".", "_");
+        var chartId = "dc-{uuid}-chart-" + propId;
+        element.append('<div style="float: left;" id="' + chartId + '"><strong>' +prop1.replace(".", "_")+prop2.replace(".", "_")+ '</strong>' +
+                       '<div style="clear: both;"></div></div>');
+        var dim = {cf}.dimension(pluck2(prop1, prop2));
+        {reducer}
+        var min = dim.bottom(1)[0][prop1];
+        var max = dim.top(1)[0][prop1] + 1;
+        var chart = dc.scatterPlot("#" + chartId);
+        chart.dimension(dim).group(group)
+            .x(d3.scale.linear().domain([min, max]))
+            .elasticY(true)
+            .elasticX(true)
+            .width({width}).height({height});
+        """.replace('{cf}', self.crossfilter_name).replace("{prop1}", self.property[0]).replace("{prop2}", self.property[1])\
+            .replace('{width}', str(self.width)).replace('{height}', str(self.height))\
+            .replace('{reducer}', self.reducerjs)
+
+class LineChart(ProperyChart):
+    def _repr_javascript_(self):
+        return """
+        var prop = "{prop}";
+        var propId = prop.replace(".", "_");
+        var chartId = "dc-{uuid}-chart-" + propId;
+        element.append('<div style="float: left;" id="' + chartId + '"><strong>' + prop + '</strong>' +
+                       '<div style="clear: both;"></div></div>');
+        var dim = {cf}.dimension(pluck(prop));
+        {reducer}
+        var min = dim.bottom(1)[0][prop];
+        var max = dim.top(1)[0][prop] + 1;
+        var chart = dc.lineChart("#" + chartId);
+        chart.dimension(dim).group(group)
+            .x(d3.scale.linear().domain([min, max]))
+            .elasticY(true)
+            .elasticX(true)
+            .width({width}).height({height}){valAcc};
+        """.replace('{cf}', self.crossfilter_name).replace("{prop}", self.property)\
+            .replace('{width}', str(self.width)).replace('{height}', str(self.height))\
+            .replace('{reducer}', self.reducerjs).replace('{valAcc}', self.value_accessorjs)
+
+class PieChart(ProperyChart):
+    def _repr_javascript_(self):
+        return """
+        var prop = "{prop}";
+        var propId = prop.replace(".", "_");
+        var chartId = "dc-{uuid}-chart-" + propId;
+        element.append('<div style="float: left;" id="' + chartId + '"><strong>' + prop + '</strong>' +
+                       '<div style="clear: both;"></div></div>');
+        var dim = {cf}.dimension(pluck(prop));
+        {reducer}
+        var min = dim.bottom(1)[0][prop];
+        var max = dim.top(1)[0][prop] + 1;
+        var chart = dc.pieChart("#" + chartId);
+        chart.dimension(dim).group(group){valAcc};
+        """.replace('{cf}', self.crossfilter_name).replace("{prop}", self.property)\
+            .replace('{width}', str(self.width)).replace('{height}', str(self.height))\
+            .replace('{reducer}', self.reducerjs).replace('{valAcc}', self.value_accessorjs)
 
 # TODO:
 #
